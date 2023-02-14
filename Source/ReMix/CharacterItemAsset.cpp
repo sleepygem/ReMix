9ac8fe55fe7f9b7488461a3951e3a7ab94f642b1
Holly Boyle <sudok8@gmail.com>
2023-02-14 19:00:44 +0000

    hell

---
+++ Source/ReMix/CharacterItemAsset.cpp
@@ -0,0 +1,21 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "CharacterItemAsset.h"
+
+FString UCharacterItemAsset::GetIdentifierString() const
+{
+	return GetPrimaryAssetId().ToString();
+}
+
+FPrimaryAssetId UCharacterItemAsset::GetPrimaryAssetId() const
+{
+	return Super::GetPrimaryAssetId();
+	/*
+	UClass* ThisClass = GetClass();
+
+	// If this is a native class use the raw name if it's a blueprint use the package name as it will be missing _C
+	FName PrimaryAssetName = ThisClass->HasAnyClassFlags(CLASS_Native | CLASS_Intrinsic) ? GetFName() : ThisClass->GetOutermost()->GetFName();
+
+	return FPrimaryAssetId(ItemType, ThisClass->GetOutermost()->GetFName());*/
+}
